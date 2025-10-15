f2eae69b0c6ac9313092e8aa6957b9354fb1af8c
rose s <ayesha03160@gmail.com>
2025-10-15 20:48:55 +0000

    Please fix tha authentication error..login error and signup authenticati

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -4,9 +4,18 @@
 import React, { ReactNode } from 'react';
 import { FirebaseProvider, FirebaseContextValue } from './provider';
 import { initializeFirebase } from './index';
-import { firebaseConfig } from './config';
+import { FirebaseOptions } from 'firebase/app';
 
 let firebaseContextValue: FirebaseContextValue | null = null;
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 function getFirebaseContextValue(): FirebaseContextValue {
   if (firebaseContextValue) {
