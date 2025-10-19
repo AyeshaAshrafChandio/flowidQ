809462f40b60ed967ef50708019a03a90385d863
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2025-10-19 19:05:16 +0500

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-3687738154-68ab9",
-  "appId": "1:205034333645:web:6ef14b69bbc1ae70ee9084",
-  "apiKey": "AIzaSyC1gfmaz0lzA_qmh5SVMlvAK2kZAcC0qe4",
-  "authDomain": "studio-3687738154-68ab9.firebaseapp.com",
-  "storageBucket": "studio-3687738154-68ab9.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "205034333645"
-};
